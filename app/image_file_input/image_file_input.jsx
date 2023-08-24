06222b1a03e77d8d652e8599e1c0f53fd2d9efa6
hamelln <vusdus91@gmail.com>
2023-08-24 15:40:16 +0900

    feat: 이미지 업로드용 input 컴포넌트 추가

---
+++ app/image_file_input/image_file_input.jsx
@@ -0,0 +1,46 @@
+import React, { memo, useRef, useState } from "react";
+import styles from "./image_file_input.module.css";
+
+const ImageFileInput = memo(({ imageUploader, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
+  const inputRef = useRef();
+  const onButtonClick = (event) => {
+    event.preventDefault();
+    inputRef.current.click();
+  };
+
+  const onChange = async (event) => {
+    setLoading(true);
+    const uploaded = await imageUploader.upload(event.target.files[0]);
+    setLoading(false);
+    const srcs = {
+      id: uploaded.public_id,
+      small: `${process.env.NEXT_PUBLIC_CLOUDINARY_API_URL}/c_scale,w_280,h_280/f_auto/q_auto/${uploaded["public_id"]}.${uploaded.format}`,
+      medium: `${process.env.NEXT_PUBLIC_CLOUDINARY_API_URL}/c_scale,w_400,h_400/f_auto/q_auto/${uploaded["public_id"]}.${uploaded.format}`,
+      large: `${process.env.NEXT_PUBLIC_CLOUDINARY_API_URL}/c_scale,w_800,h_800/f_auto/q_auto/${uploaded["public_id"]}.${uploaded.format}`,
+    };
+    onFileChange(srcs);
+  };
+
+  return (
+    <div className={styles.container}>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="file"
+        accept="image/*"
+        multiple
+        name="file"
+        onChange={onChange}
+      />
+      {!loading && (
+        <button
+          className={`${styles.button} ${styles.pink}`}
+          onClick={onButtonClick}
+        ></button>
+      )}
+      {loading && <div className={styles.loading}></div>}
+    </div>
+  );
+});
+export default ImageFileInput;
