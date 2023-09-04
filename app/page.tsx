fcc67b18b032520837f37fd316303b05e9b1898a
hamelln <vusdus91@gmail.com>
2023-09-04 21:08:04 +0900

    fix: 코드 임시 변경

--- app/page.tsx
+++ app/page.tsx
@@ -86,6 +86,7 @@ const page = ({ brewery }: Props) => {
   const days: string[] = ["월", "화", "수", "목", "금", "토", "일"];
   const breweriesApi = new BreweriesApi();
   const imageUploader = new ImageUploader();
+  const fileReader = new FileReader();
 
   const FileInput = (props: any) => (
     <ImageFileInput {...props} imageUploader={imageUploader} />
@@ -148,251 +149,242 @@ const page = ({ brewery }: Props) => {
   };
 
   const deleteBrewery = async () => {
-    // await breweriesApi.deleteBrewery(breweryData.id);
+    await breweriesApi.deleteBrewery(brewery.id);
   };
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
-
-    // const updatedSignatureBeer = {
-    //   signatureBeerName,
-    //   signatureBeerDescription,
-    // };
-
-    // const updatedBrewery = {
-    //   ...breweryData,
-    //   breweryName,
-    //   breweryDescription,
-    //   stateProvince,
-    //   city,
-    //   address,
-    //   phone,
-    //   websiteUrl,
-    //   images: breweryImages,
-    //   officeHours,
-    //   signatureBeer: updatedSignatureBeer,
-    // };
-
-    // await breweriesApi.updateBrewery(updatedBrewery);
+    await breweriesApi.updateBrewery(formData);
   };
 
   return (
-    <form className={S.section} onSubmit={handleSubmit}>
-      <h1>가게 정보 수정</h1>
-      <div className={S.info_box}>
-        <div className={S.name_box}>
-          <span>이름</span>
+    <section className={S.section}>
+      <form className={S.form} onSubmit={handleSubmit}>
+        <h1>가게 정보 수정</h1>
+        <div className={S.info_box}>
+          <div className={S.name_box}>
+            <span>이름</span>
+          </div>
+          <div className={S.input_box}>
+            <input
+              type="text"
+              placeholder="이름"
+              value={formData.breweryName}
+              name="breweryName"
+              onChange={handleInput}
+            />
+          </div>
         </div>
-        <div className={S.input_box}>
-          <input
-            type="text"
-            placeholder="이름"
-            value={formData.breweryName}
-            name="breweryName"
-            onChange={handleInput}
-          />
+        <div className={S.info_box}>
+          <div className={S.name_box}>
+            <span>사진</span>
+          </div>
+          <ul className={S.image_box}>
+            {formData.images.map((image: string, i: number) => {
+              return (
+                <li key={i} className={S.image}>
+                  <Image
+                    src={image}
+                    alt="image"
+                    width={200}
+                    height={200}
+                  ></Image>
+                  <button
+                    className={S.delete_button}
+                    onClick={() => {
+                      handleDelete(i);
+                    }}
+                  >
+                    X
+                  </button>
+                </li>
+              );
+            })}
+            <button className={S.upload_button}>업로드</button>
+          </ul>
         </div>
-      </div>
-      <div className={S.info_box}>
-        <div className={S.name_box}>
-          <span>사진</span>
+        <div className={S.info_box}>
+          <div className={S.name_box}>
+            <span>주소</span>
+          </div>
+          <div className={S.input_box}>
+            <input
+              type="text"
+              placeholder="주소"
+              name="address"
+              value={formData.fullAddress}
+              onChange={handleInput}
+            />
+            <div>도, 특별시, 광역시: {formData.stateProvince}</div>
+            <div>시군구: {formData.city}</div>
+          </div>
         </div>
-        <ul className={S.image_box}>
-          {formData.images.map((image: string, i: number) => {
-            return (
-              <li key={i} className={S.image}>
-                <Image src={image} alt="image" width={200} height={200}></Image>
-                <button
-                  className={S.delete_button}
-                  onClick={() => {
-                    handleDelete(i);
-                  }}
-                >
-                  X
-                </button>
-              </li>
-            );
-          })}
-          <button className={S.upload_button}>업로드</button>
-        </ul>
-      </div>
-      <div className={S.info_box}>
-        <div className={S.name_box}>
-          <span>주소</span>
-        </div>
-        <div className={S.input_box}>
-          <input
-            type="text"
-            placeholder="주소"
-            name="address"
-            value={formData.fullAddress}
-            onChange={handleInput}
-          />
-          <div>도, 특별시, 광역시: {formData.stateProvince}</div>
-          <div>시군구: {formData.city}</div>
-        </div>
-      </div>
 
-      <div className={S.info_box}>
-        <div className={S.name_box}>
-          <span>전화번호</span>
+        <div className={S.info_box}>
+          <div className={S.name_box}>
+            <span>전화번호</span>
+          </div>
+          <div className={S.input_box}>
+            <input
+              type="text"
+              placeholder="전화번호"
+              name="phone"
+              value={formData.phone}
+              maxLength={14}
+              onChange={handleInput}
+            />
+          </div>
         </div>
-        <div className={S.input_box}>
-          <input
-            type="text"
-            placeholder="전화번호"
-            name="phone"
-            value={formData.phone}
-            maxLength={14}
-            onChange={handleInput}
-          />
+        <div className={S.info_box}>
+          <div className={S.name_box}>
+            <span>홈페이지, SNS</span>
+          </div>
+          <div className={S.input_box}>
+            <input
+              type="text"
+              placeholder="홈페이지, SNS"
+              value={formData.websiteUrl}
+              name="websiteUrl"
+              onChange={handleInput}
+            />
+            <div>사이트 타입: {formData.websiteType}</div>
+          </div>
         </div>
-      </div>
-      <div className={S.info_box}>
-        <div className={S.name_box}>
-          <span>홈페이지, SNS</span>
+        <div className={S.info_box}>
+          <div className={S.name_box}>
+            <span>가게 한 줄 소개</span>
+          </div>
+          <div className={S.input_box}>
+            <input
+              type="text"
+              placeholder="가게 한 줄 소개"
+              name="breweryIntro"
+              value={formData.breweryIntro}
+              onChange={handleInput}
+            />
+          </div>
         </div>
-        <div className={S.input_box}>
-          <input
-            type="text"
-            placeholder="홈페이지, SNS"
-            value={formData.websiteUrl}
-            name="websiteUrl"
-            onChange={handleInput}
-          />
-          <div>사이트 타입: {formData.websiteType}</div>
+        <div className={S.info_box}>
+          <div className={S.name_box}>
+            <span>가게 전체 소개</span>
+          </div>
+          <div className={S.input_box}>
+            <textarea
+              placeholder="가게 설명"
+              name="breweryDescription"
+              value={formData.breweryDescription}
+              onChange={handleInput}
+            />
+          </div>
         </div>
-      </div>
-      <div className={S.info_box}>
-        <div className={S.name_box}>
-          <span>가게 한 줄 소개</span>
+        <div className={S.info_box}>
+          <div className={S.name_box}>
+            <span>추천 맥주 이름</span>
+          </div>
+          <div className={S.input_box}>
+            <input
+              type="text"
+              placeholder="추천 맥주 이름"
+              name="beerName"
+              value={formData.beerName}
+              onChange={handleInput}
+            />
+          </div>
         </div>
-        <div className={S.input_box}>
-          <input
-            type="text"
-            placeholder="가게 한 줄 소개"
-            name="breweryIntro"
-            value={formData.breweryIntro}
-            onChange={handleInput}
-          />
+        <div className={S.info_box}>
+          <div className={S.name_box}>
+            <span>추천 맥주 설명</span>
+          </div>
+          <div className={S.input_box}>
+            <textarea
+              placeholder="추천 맥주 설명"
+              name="beerDescription"
+              value={formData.beerDescription}
+              onChange={handleInput}
+            />
+          </div>
         </div>
-      </div>
-      <div className={S.info_box}>
-        <div className={S.name_box}>
-          <span>가게 전체 소개</span>
-        </div>
-        <div className={S.input_box}>
-          <textarea
-            placeholder="가게 설명"
-            name="breweryDescription"
-            value={formData.breweryDescription}
-            onChange={handleInput}
-          />
-        </div>
-      </div>
-      <div className={S.info_box}>
-        <div className={S.name_box}>
-          <span>추천 맥주 이름</span>
-        </div>
-        <div className={S.input_box}>
-          <input
-            type="text"
-            placeholder="추천 맥주 이름"
-            name="beerName"
-            value={formData.beerName}
-            onChange={handleInput}
-          />
-        </div>
-      </div>
-      <div className={S.info_box}>
-        <div className={S.name_box}>
-          <span>추천 맥주 설명</span>
-        </div>
-        <div className={S.input_box}>
-          <textarea
-            placeholder="추천 맥주 설명"
-            name="beerDescription"
-            value={formData.beerDescription}
-            onChange={handleInput}
-          />
-        </div>
-      </div>
-      <div className={S.info_box}>
-        <div className={S.name_box}>
-          <span>영업 시간</span>
-        </div>
-        <div className={S.input_box}>
-          <div>
+        <div className={S.info_box}>
+          <div className={S.name_box}>
+            <span>영업 시간</span>
+          </div>
+          <div className={S.input_box}>
             <div>
-              {days.map((day) => (
-                <button
-                  key={day}
-                  onClick={handleDayClick}
-                  className={selectedDay === day ? S.selected : ""}
-                >
-                  {day}
-                </button>
-              ))}
-            </div>
-            <div>
-              <div className={S.time_box}>
-                <input
-                  type="text"
-                  placeholder="00:00"
-                  value={formData.officeHours[selectedDay].openTime}
-                  name="openTime"
-                  onChange={handleInput}
-                />{" "}
-                ~
-                <input
-                  type="text"
-                  placeholder="00:00"
-                  name="closeTime"
-                  value={formData.officeHours[selectedDay].closeTime}
-                  onChange={handleInput}
-                />
+              <div>
+                {days.map((day) => (
+                  <button
+                    key={day}
+                    onClick={handleDayClick}
+                    className={selectedDay === day ? S.selected : ""}
+                  >
+                    {day}
+                  </button>
+                ))}
               </div>
-              <div className={S.time_box}>
-                <input
-                  type="text"
-                  placeholder="00:00"
-                  value={formData.officeHours[selectedDay].breakTime.startTime}
-                  name="startTime"
-                  onChange={handleInput}
-                />{" "}
-                ~
-                <input
-                  type="text"
-                  placeholder="00:00"
-                  name="endTime"
-                  value={formData.officeHours[selectedDay].breakTime.endTime}
-                  onChange={handleInput}
-                />
-              </div>
-              <div className={S.time_box}>
-                <input
-                  type="text"
-                  placeholder="00:00"
-                  value={formData.officeHours[selectedDay].lastOrder}
-                  name="lastOrder"
-                  onChange={handleInput}
-                />
+              <div>
+                <div className={S.time_box}>
+                  <input
+                    type="text"
+                    placeholder="00:00"
+                    value={formData.officeHours[selectedDay].openTime}
+                    name="openTime"
+                    onChange={handleInput}
+                  />{" "}
+                  ~
+                  <input
+                    type="text"
+                    placeholder="00:00"
+                    name="closeTime"
+                    value={formData.officeHours[selectedDay].closeTime}
+                    onChange={handleInput}
+                  />
+                </div>
+                <div className={S.time_box}>
+                  <input
+                    type="text"
+                    placeholder="00:00"
+                    value={
+                      formData.officeHours[selectedDay].breakTime.startTime
+                    }
+                    name="startTime"
+                    onChange={handleInput}
+                  />{" "}
+                  ~
+                  <input
+                    type="text"
+                    placeholder="00:00"
+                    name="endTime"
+                    value={formData.officeHours[selectedDay].breakTime.endTime}
+                    onChange={handleInput}
+                  />
+                </div>
+                <div className={S.time_box}>
+                  <input
+                    type="text"
+                    placeholder="00:00"
+                    value={formData.officeHours[selectedDay].lastOrder}
+                    name="lastOrder"
+                    onChange={handleInput}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <button type="submit" style={{ color: "var(--color-font-primary)" }}>
-        정보 수정하기
-      </button>
-      <button
-        type="button"
-        onClick={deleteBrewery}
-        style={{ color: "var(--color-font-primary)" }}
-      >
-        가게 정보 삭제
-      </button>
-    </form>
+        <div className={S.submit_box}>
+          <button type="submit" className={S.submit_button}>
+            가게 수정
+          </button>
+          <button
+            type="button"
+            onClick={deleteBrewery}
+            className={S.delete_brewery}
+          >
+            가게 삭제
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
